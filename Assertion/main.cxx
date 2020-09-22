e8b128b3d5d73868f415823633fa45f1676d93b9
Pablo Ariza Luna <pablo_ariza@javeriana.edu.co>
2020-09-22 07:51:44 -0500

    static assertion with templates

--- Assertion/main.cxx
+++ Assertion/main.cxx
@@ -1,12 +1,24 @@
 #define _ASSERT_GLUE(a, b) a ## b
 #define ASSERT_GLUE(a, b) _ASSERT_GLUE(a, b)
 
-#define STATIC_ASSERT(expr) \
-    enum \
-    { \
-        ASSERT_GLUE(g_assert_fail_, __LINE__) \
-        = 1 / (int)(!!(expr)) \
-    }
+#ifdef __cplusplus
+    #if __cplusplus >= 201103L
+        #define STATIC_ASSERT(expr) \
+            static_assert(expr, \
+                          "static assert failed:" \
+                          #expr)
+    #else
+        template<bool> class TStaticAssert;
+        template<> class TStaticAssert<true> {};
+
+        #define STATIC_ASSERT(expr) \
+            enum \
+            { \
+                ASSERT_GLUE(g_assert_fail_, __LINE__) \
+                    = sizeof(TStaticAssert<!!(expr)>) \
+            }
+    #endif
+#endif
 
 int main() {
     STATIC_ASSERT(sizeof(int) == 4);
